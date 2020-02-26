a0fa44bdb9a514fabe442f1192e16785c109947b
ntepluhina <ntepluhina@gitlab.com>
2020-02-26 19:38:45 +0200

    feat: implemented REST fetch

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,7 +1,10 @@
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import axios from 'axios';
 import favoriteCharactersQuery from './queries/favoriteCharacters.query.gql';
+
+axios.defaults.baseURL = 'https://rickandmortyapi.com/api/';
 
 const httpLink = createHttpLink({
   uri: 'https://rickandmortyapi.com/graphql'
@@ -10,10 +13,23 @@ const httpLink = createHttpLink({
 const cache = new InMemoryCache();
 
 const resolvers = {
+  Query: {
+    characters() {
+      return axios.get('/character').then(res => ({
+        results: res.data.results.map(char => ({
+          __typename: 'Character',
+          id: char.id,
+          name: char.name,
+          location: char.location,
+          image: char.image
+        }))
+      }));
+    }
+  },
   Mutation: {
     addToFavorites(_, { character }, { cache }) {
       const data = cache.readQuery({ query: favoriteCharactersQuery });
-      data.favoriteCharacters.push(character);
+      data.favoriteCharacters = [...data.favoriteCharacters, character];
       cache.writeQuery({ query: favoriteCharactersQuery, data });
     },
     removeFromFavorites(_, { id }, { cache }) {
