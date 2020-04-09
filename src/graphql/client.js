70ca44939b4b3f0443a0a1b61b058b6014eee88c
NataliaTepluhina <tarya.se@gmail.com>
2020-04-09 08:22:07 +0300

    Added typeDefs
    Conflicts:
    	src/graphql/client.js

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -2,20 +2,23 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import axios from 'axios';
+import gql from 'graphql-tag';
 import favoriteCharactersQuery from './queries/favoriteCharacters.query.gql';
 
 axios.defaults.baseURL = 'https://rickandmortyapi.com/api';
 
 const httpLink = createHttpLink({
-  uri: 'https://rickandmortyapi.com/graphql'
+  uri: 'https://rickandmortyapi.com/graphql',
 });
 
 const cache = new InMemoryCache();
 
 const resolvers = {};
 
+cache.writeData({ data: { favoriteCharacters: [] } });
+
 export const apolloClient = new ApolloClient({
   link: httpLink,
   cache,
-  resolvers
+  resolvers,
 });
