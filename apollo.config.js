a54c8d58d99535d76a7266acf1da680b966bb4ed
NataliaTepluhina <tarya.se@gmail.com>
2020-04-09 08:22:07 +0300

    Added typeDefs

---
+++ apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'Rick and Morty',
+      url: 'https://rickandmortyapi.com/graphql',
+    },
+  },
+};
--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,13 +1,32 @@
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
 import favoriteCharactersQuery from './queries/favoriteCharacters.query.gql';
 
 const httpLink = createHttpLink({
-  uri: 'https://rickandmortyapi.com/graphql'
+  uri: 'https://rickandmortyapi.com/graphql',
 });
 
 const cache = new InMemoryCache();
+
+const typeDefs = gql`
+  input CharacterInput {
+    id: ID!
+    name: String
+    image: String
+    location: Location
+  }
+
+  extend type Query {
+    favoriteCharacters: [Character]!
+  }
+
+  type Mutation {
+    addToFavorites(character: CharacterInput!): [Character!]!
+    removeFromFavorites(id: ID!): Boolean
+  }
+`;
 
 const resolvers = {
   Mutation: {
@@ -19,11 +38,11 @@ const resolvers = {
     removeFromFavorites(_, { id }, { cache }) {
       const data = cache.readQuery({ query: favoriteCharactersQuery });
       data.favoriteCharacters = data.favoriteCharacters.filter(
-        character => character.id !== id
+        (character) => character.id !== id
       );
       cache.writeQuery({ query: favoriteCharactersQuery, data });
-    }
-  }
+    },
+  },
 };
 
 cache.writeData({ data: { favoriteCharacters: [] } });
@@ -31,5 +50,6 @@ cache.writeData({ data: { favoriteCharacters: [] } });
 export const apolloClient = new ApolloClient({
   link: httpLink,
   cache,
-  resolvers
+  resolvers,
+  typeDefs,
 });
